c0bb45706026aaa8d329d1172a6122b3bbc10cc2
sandeepmaram <sandeepmaram@gmail.com>
2023-04-27 08:34:40 +0530

    Initial commit

---
+++ app/src/main/java/com/msr/composedemo/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.msr.composedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.msr.composedemo.ui.components.PieChart
+import com.msr.composedemo.ui.theme.CustomPieChartJCTheme
+import com.msr.composedemo.ui.theme.JetpackComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*setContent {
+            JetpackComposeDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }*/
+        setContent {
+            CustomPieChartJCTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    // Preview with sample data
+                    PieChart(
+                        data = mapOf(
+                            Pair("Sample-1", 150),
+                            Pair("Sample-2", 120),
+                            Pair("Sample-3", 110),
+                            Pair("Sample-4", 170),
+                            Pair("Sample-5", 120),
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+data class ChartItem(val value: Float, val color: Color, val label: String = "")
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeDemoTheme {
+        Greeting("Android")
+    }
+}
